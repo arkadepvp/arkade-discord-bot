0cd20704ab0872e9ee3fd55563a3f0f7934dad9e
N0XIRE <connorwilson989@gmail.com>
2018-10-29 18:01:43 -0500

    Fixed invalid attempt to find client.event

--- donate.py
+++ donate.py
@@ -2,15 +2,12 @@ import discord
 import asyncio
 from discord.ext import commands
 from discord.ext.commands import Bot
-from lxml import html
-import requests
-import sys
 
 class donate:
     def __init__(self, client):
         self.client = client
 
-    @client.event
+    @commands.event
     async def on_ready():
         message = await client.send_message(502380383434833920, "test")
 
