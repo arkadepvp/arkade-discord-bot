be6a14fc10efcb4a7a33c4149534ea75097569e2
N0XIRE <connorwilson989@gmail.com>
2018-10-29 18:11:38 -0500

    fix to on_ready(self)

--- donate.py
+++ donate.py
@@ -7,8 +7,8 @@ class donate:
     def __init__(self, client):
         self.client = client
 
-    async def on_ready():
-        message = await client.send_message(502380383434833920, "test")
+    async def on_ready(self):
+        message = await self.client.send_message(502380383434833920, "test")
 
 def setup(client):
     client.add_cog(donate(client))
