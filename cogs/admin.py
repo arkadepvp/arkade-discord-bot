e4aeaae9235a1dc1609b84af8b9ac3a80df33717
N0XIRE <connorwilson989@gmail.com>
2018-11-06 11:27:23 -0600

    Updated welcome message for s3

--- cogs/admin.py
+++ cogs/admin.py
@@ -54,12 +54,12 @@ class admin:
 
         embed = discord.Embed(title="Welcome to Arkade PvP!", description="```md\nArkade PvP - More than just a server.\n-------------------------------------\n```\n", color=0xCC33CC)
         embed.set_image(url="https://cdn.discordapp.com/attachments/461022949798051871/472549308454010880/arkadebanner23.png")
-        embed.add_field(name="Rates", value="✦ EXP: 3x\n✦ Gathering: 5x\n✦ Taming: 5x\n✦ Breeding: 10x\n✦ Character Level: 104 +30\n✦ Wild Dino Level: 150", inline="true")
+        embed.add_field(name="Rates", value="✦ EXP: 3x\n✦ Gathering: 5x\n✦ Taming: 7x\n✦ Breeding: 10x\n✦ Character Level: 104 +30\n✦ Wild Dino Level: 150", inline="true")
         embed.add_field(name="Links", value="✦ [Our Website](http://arkadepvp.com)\n✦ [Guilded](https://www.guilded.gg/ArkadePvP/)\n✦ [Discord](https://discord.me/arkade)", inline="true")
         embed.add_field(name="_ _", value="_ _")
         embed.add_field(name="Quick Information", value="✦ Please read <#492888582726615040> for our rules and config settings.\n✦ Check <#472558681687457792> for our connect and vote links.\n✦ Ping @Arkade Admin in <#472623301290622993> if there is a server crash.")
         embed.add_field(name="_ _", value="_ _")
-        embed.add_field(name="Server Direct Connect Links", value="✦ Ragnarok: steam://connect/147.135.8.214:27015\n✦ Aberration: steam://connect/147.135.9.6:27015\n✦ The Center: steam://connect/147.135.8.214:27017\n✦ The Island: steam://connect/147.135.8.214:27018\n✦ Scorched Earth: steam://connect/147.135.9.6:27016\n✦ Event Map: steam://connect/147.135.9.6:27051\n")
+        embed.add_field(name="Server Direct Connect Links", value="✦ Extinction: steam://connect/147.135.9.6:27017\n✦ Ragnarok: steam://connect/147.135.8.214:27015\n✦ Aberration: steam://connect/147.135.9.6:27015\n✦ The Center: steam://connect/147.135.8.214:27017\n✦ The Island: steam://connect/147.135.8.214:27018\n✦ Scorched Earth: steam://connect/147.135.9.6:27016\n✦ Event Map: steam://connect/147.135.9.6:27051\n")
         message = await ctx.message.channel.send(embed=embed)
 
 def setup(client):
