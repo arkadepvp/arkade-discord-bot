46590aa8fe1939ea6a17635003ba1447533cf9c3
N0XIRE <connorwilson989@gmail.com>
2018-12-14 19:35:35 -0600

    Fixed faulty path

--- cogs/shoplogs.py
+++ cogs/shoplogs.py
@@ -38,7 +38,7 @@ class shopcharts:
         time.sleep(.1)
         urllib.request.urlretrieve('ftp://' + ftp2 + '@147.135.9.6/Servers/S3C1M2-L3P-Scorched/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_ScorchedEarth_P.log', 'shoplogs/ShopLog_ScorchedEarth.log')
         time.sleep(.1)
-        urllib.request.urlretrieve('ftp://' + ftp4 + '@147.135.30.58/Servers/S3C1M1-L2P-Center/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_TheCenter.log', 'shoplogs/ShopLog_TheCenter.log')
+        urllib.request.urlretrieve('ftp://' + ftp4 + '@147.135.30.58/ArkServers/Servers/S3C1M4-L1P-Center/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_TheCenter.log', 'shoplogs/ShopLog_TheCenter.log')
         time.sleep(.1)
         urllib.request.urlretrieve('ftp://' + ftp3 + '@147.135.8.210/Servers/S3C1M3-L1P-Extinction2/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_Extinction.log', 'shoplogs/ShopLog_Ext2.log')
         time.sleep(.1)
