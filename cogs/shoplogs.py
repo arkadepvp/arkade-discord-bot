e46a573942d831137375f7d440f49635bd23aa19
N0XIRE <connorwilson989@gmail.com>
2018-12-14 19:29:41 -0600

    Updated shoplogs to include server 4

--- cogs/shoplogs.py
+++ cogs/shoplogs.py
@@ -27,17 +27,18 @@ class shopcharts:
         ftp1 = config['ftp1user'] + ':' + config['ftp1pass']
         ftp2 = config['ftp2user'] + ':' + config['ftp2pass']
         ftp3 = config['ftp3user'] + ':' + config['ftp3pass']
+        ftp4 = config['ftp4user'] + ':' + config['ftp4pass']
 
         #download from ArkadePvP server
         urllib.request.urlretrieve('ftp://' + ftp1 + '@147.135.8.214/Servers/S3C1M1-L1P-Ragnarok/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_Ragnarok.log', 'shoplogs/ShopLog_Ragnarok.log')
         time.sleep(.1)
-        urllib.request.urlretrieve('ftp://' + ftp2 + '@147.135.9.6/Servers/S3C1M2-L2P-Aberration/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_Aberration_P.log', 'shoplogs/ShopLog_Aberration.log')
-        time.sleep(.1)
-        urllib.request.urlretrieve('ftp://' + ftp1 + '@147.135.8.214/Servers/S3C1M1-L2P-Center/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_TheCenter.log', 'shoplogs/ShopLog_TheCenter.log')
-        time.sleep(.1)
         urllib.request.urlretrieve('ftp://' + ftp1 + '@147.135.8.214/Servers/S3C1M1-L3P-Island/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_TheIsland.log', 'shoplogs/ShopLog_TheIsland.log')
         time.sleep(.1)
+        urllib.request.urlretrieve('ftp://' + ftp2 + '@147.135.9.6/Servers/S3C1M2-L2P-Aberration/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_Aberration_P.log', 'shoplogs/ShopLog_Aberration.log')
+        time.sleep(.1)
         urllib.request.urlretrieve('ftp://' + ftp2 + '@147.135.9.6/Servers/S3C1M2-L3P-Scorched/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_ScorchedEarth_P.log', 'shoplogs/ShopLog_ScorchedEarth.log')
+        time.sleep(.1)
+        urllib.request.urlretrieve('ftp://' + ftp4 + '@147.135.30.58/Servers/S3C1M1-L2P-Center/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_TheCenter.log', 'shoplogs/ShopLog_TheCenter.log')
         time.sleep(.1)
         urllib.request.urlretrieve('ftp://' + ftp3 + '@147.135.8.210/Servers/S3C1M3-L1P-Extinction2/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_Extinction.log', 'shoplogs/ShopLog_Ext2.log')
         time.sleep(.1)
