02160120881daf6b797444636244a538f55433c0
N0XIRE <connorwilson989@gmail.com>
2018-12-22 04:33:13 -0600

    Hopefully last formatting fix

--- cogs/admin.py
+++ cogs/admin.py
@@ -92,7 +92,7 @@ class admin:
     async def welcome(self, ctx):
         await ctx.message.delete()
 
-        embed = discord.Embed(title="Welcome to Arkade!", description="A PvP and PvE ARK community.\n\n", color=0xCC33CC)
+        embed = discord.Embed(title="Welcome to Arkade!", description="**A PvP and PvE ARK community.**\n_ _", color=0xCC33CC)
         embed.set_image(url="https://cdn.discordapp.com/attachments/461022949798051871/472549308454010880/arkadebanner23.png")
         embed.add_field(name="Rates", value="✦ EXP: 3x\n✦ Gathering: 5x\n✦ Taming: 7x\n✦ Breeding: 10x (15x PvE)\n✦ Character Level: 105 +30\n✦ Wild Dino Level: 150", inline="true")
         embed.add_field(name="Links", value="✦ [Our Website](http://arkadepvp.com)\n✦ [Donate](https://shop.arkadepvp.com/)\n✦ [Guilded](https://www.guilded.gg/ArkadePvP/)\n✦ [Discord](https://discord.me/arkade)\n✦ [Twitch](https://www.twitch.tv/tiamoarkade)", inline="true")
