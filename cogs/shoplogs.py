6eb07d87856aa7765ce1f01294f755628f24fc62
N0XIRE <connorwilson989@gmail.com>
2018-12-30 14:03:55 -0600

    Removed arbritrary config options

--- cogs/shoplogs.py
+++ cogs/shoplogs.py
@@ -23,22 +23,15 @@ class shopcharts:
         await ctx.message.delete()
         #COLLECT DATA
 
-        #Sets user and pass vars.
-        ftp1 = config['ftp1user'] + ':' + config['ftp1pass']
-        ftp2 = config['ftp2user'] + ':' + config['ftp2pass']
-        ftp3 = config['ftp3user'] + ':' + config['ftp3pass']
-        ftp4 = config['ftp4user'] + ':' + config['ftp4pass']
-        ftp5 = config['ftp5user'] + ':' + config['ftp5pass']
-
         if string == "pvp":
             #download from ArkadePvP server
-            urllib.request.urlretrieve('ftp://' + ftp1 + '@147.135.8.214/Servers/S3C1M1-L1P-Ragnarok/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_Ragnarok.log', 'shoplogs/PvP_Ragnarok.log')
-            urllib.request.urlretrieve('ftp://' + ftp1 + '@147.135.8.214/Servers/S3C1M1-L3P-Island/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_TheIsland.log', 'shoplogs/PvP_TheIsland.log')
-            urllib.request.urlretrieve('ftp://' + ftp2 + '@147.135.9.6/Servers/S3C1M2-L2P-Aberration/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_Aberration_P.log', 'shoplogs/PvP_Aberration.log')
-            urllib.request.urlretrieve('ftp://' + ftp2 + '@147.135.9.6/Servers/S3C1M2-L3P-Scorched/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_ScorchedEarth_P.log', 'shoplogs/PvP_ScorchedEarth.log')
-            urllib.request.urlretrieve('ftp://' + ftp4 + '@147.135.30.58/Servers/S3C1M4-L1P-Center/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_TheCenter.log', 'shoplogs/PvP_TheCenter.log')
-            urllib.request.urlretrieve('ftp://' + ftp3 + '@147.135.8.210/Servers/S3C1M3-L1P-Extinction2/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_Extinction.log', 'shoplogs/PvP_Ext2.log')
-            urllib.request.urlretrieve('ftp://' + ftp2 + '@147.135.9.6/Servers/S3C1M2-L1P-Extinction/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_Extinction.log', 'shoplogs/PvP_Ext1.log')
+            urllib.request.urlretrieve('ftp://' + config['ftp1'] + '@147.135.8.214/Servers/S3C1M1-L1P-Ragnarok/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_Ragnarok.log', 'shoplogs/PvP_Ragnarok.log')
+            urllib.request.urlretrieve('ftp://' + config['ftp1'] + '@147.135.8.214/Servers/S3C1M1-L3P-Island/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_TheIsland.log', 'shoplogs/PvP_TheIsland.log')
+            urllib.request.urlretrieve('ftp://' + config['ftp2'] + '@147.135.9.6/Servers/S3C1M2-L2P-Aberration/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_Aberration_P.log', 'shoplogs/PvP_Aberration.log')
+            urllib.request.urlretrieve('ftp://' + config['ftp2'] + '@147.135.9.6/Servers/S3C1M2-L3P-Scorched/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_ScorchedEarth_P.log', 'shoplogs/PvP_ScorchedEarth.log')
+            urllib.request.urlretrieve('ftp://' + config['ftp4'] + '@147.135.30.58/Servers/S3C1M4-L1P-Center/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_TheCenter.log', 'shoplogs/PvP_TheCenter.log')
+            urllib.request.urlretrieve('ftp://' + config['ftp3'] + '@147.135.8.210/Servers/S3C1M3-L1P-Extinction2/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_Extinction.log', 'shoplogs/PvP_Ext2.log')
+            urllib.request.urlretrieve('ftp://' + config['ftp2'] + '@147.135.9.6/Servers/S3C1M2-L1P-Extinction/ShooterGame/Binaries/Win64/ArkApi/Plugins/ArkShop/ShopLog_Extinction.log', 'shoplogs/PvP_Ext1.log')
 
             filenames = ['shoplogs/PvP_Ragnarok.log', 'shoplogs/PvP_Aberration.log', 'shoplogs/PvP_TheCenter.log', 'shoplogs/PvP_TheIsland.log', 'shoplogs/PvP_ScorchedEarth.log', 'shoplogs/PvP_Ext1.log', 'shoplogs/PvP_Ext2.log']
             with open('shoplogs/PvP_Combined.log', 'w') as outfile:
