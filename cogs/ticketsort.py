2539bfecc7aa668b8ca11f78eb05b911d461ed7a
N0XIRE <connorwilson989@gmail.com>
2019-01-19 04:02:20 -0600

    Removed diagnostic print and if logic change

--- cogs/ticketsort.py
+++ cogs/ticketsort.py
@@ -9,12 +9,11 @@ class ticketsort:
 
     async def on_guild_channel_update(self, before, after):
         if after.category_id == 473652853354004480:
-            print("New Ticket Created")
             if "are" in after.name:
                 areCategory = discord.utils.get(after.guild.categories, id=533147428715495434)
                 await after.edit(category=areCategory, topic="ARK PvE Ticket")
                 print("Ticket moved to PvE with ID: " + str(areCategory.id))
-            if "sc" in after.name:
+            elif "sc" in after.name:
                 scCategory = discord.utils.get(after.guild.categories, id=533752291853860864)
                 await after.edit(category=scCategory, topic="ARK SC Ticket")
                 print("Ticket moved to SC with ID: " + str(scCategory.id))
