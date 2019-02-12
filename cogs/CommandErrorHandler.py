6b5b8f6ea2f344772cec2dcd925e1ceaf2bbdae8
N0XIRE <connorwilson989@gmail.com>
2019-02-12 07:31:07 -0600

    Removed error handling until bugs can be worked out

--- cogs/CommandErrorHandler.py
+++ cogs/CommandErrorHandler.py
@@ -8,28 +8,5 @@ class CommandErrorHandler:
     def __init__(self, client):
         self.client = client
 
-    async def on_command_error(self, ctx, error):
-        if hasattr(ctx.command, 'on_error'):
-            return
-        ignore = (commands.CommandNotFound, commands.UserInputError, commands.DisabledCommand)
-
-        error = getattr(error, 'original', error)
-
-        if isinstance(error, ignore):
-            return
-
-        elif isinstance(error, commands.NoPrivateMessage):
-            try:
-                return await ctx.author.send(f'{ctx.command} can not be used in Private Messages.')
-            except:
-                pass
-
-        elif isinstance(error, commands.BadArgument):
-            return await ctx.send('Error: Invalid Argument')
-
-        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
-
-
 def setup(client):
     client.add_cog(CommandErrorHandler(client))
