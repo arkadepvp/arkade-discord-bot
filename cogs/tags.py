1518d6a9bc32272d1b789dd11858f2556c7a6442
N0XIRE <connorwilson989@gmail.com>
2019-02-12 20:29:40 -0600

    Added tags command embed reporting

--- cogs/tags.py
+++ cogs/tags.py
@@ -21,9 +21,8 @@ class info:
         if search is None:
             embed = discord.Embed(title="ERROR", description="No tag specified.", color=0xFF0000)
         else:
-            search = search.lower()
             for tag in tags:
-                if search in tag:
+                if search.lower() in tag.lower():
                     tagvars = tags[tag]
                     embed = discord.Embed(title=tagvars['title'], description=tagvars['desc'], color=0x589BFF)
                     if tagvars['image'] is not "":
@@ -33,7 +32,7 @@ class info:
                             embed.add_field(name=field['title'], value=field['value'])
                     break
                 else:
-                    embed = discord.Embed(title="ERROR", description="No results found.", color=0xFF0000)
+                    embed = discord.Embed(title="ERROR", description="No results found for: `" + search + "`", color=0xFF0000)
 
         message = await ctx.message.channel.send(embed=embed)
 
@@ -44,8 +43,9 @@ class info:
 
         tagString = ""
         for tag in tags:
-            tagString = tagString + "\n"
-        print(tagString)
+            tagString = tagString + "- " + tag + "\n"
+        embed = discord.Embed(title="Available Tags List", description=tagString, color=0x589BFF)
+        message = await ctx.message.channel.send(embed=embed)
 
 
 def setup(client):
