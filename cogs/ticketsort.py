74c5bb6eac5b2ef3b731a68e1d030adb40f6cf85
N0XIRE <connorwilson989@gmail.com>
2019-03-21 22:55:25 -0500

    maybe really fixed

--- cogs/ticketsort.py
+++ cogs/ticketsort.py
@@ -10,11 +10,11 @@ class ticketsort:
 
     async def on_guild_channel_update(self, before, after):
         if after.category_id == 473652853354004480:
-            if "are" in after.name[0:10]:
+            if "are" in after.name[0:11]:
                 areCategory = discord.utils.get(after.guild.categories, id=533147428715495434)
                 await after.edit(category=areCategory, topic="ARK PvE Ticket")
                 print("Ticket moved to PvE with ID: " + str(areCategory.id))
-            elif "arp" in after.name[0:10]:
+            elif "arp" in after.name[0:11]:
                 scCategory = discord.utils.get(after.guild.categories, id=533752291853860864)
                 await after.edit(category=scCategory, topic="ARK PvP Ticket")
                 print("Ticket moved to PvP with ID: " + str(scCategory.id))
