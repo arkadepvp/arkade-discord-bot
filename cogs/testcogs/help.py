e528150062a6ce4b0748b70295ac03524f8b4374
N0XIRE <connorwilson989@gmail.com>
2021-08-11 22:25:29 -0500

    put old help into testcogs (temporary)

---
+++ cogs/testcogs/help.py
@@ -0,0 +1,19 @@
+import discord
+import asyncio
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+class help(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        word = 'help'
+        if word.lower() in message.content.lower():
+            await message.channel.send(f'Your message contained the word "{word}". To access the help bot please type -help')
+
+def setup(client):
+    client.add_cog(help(client))
